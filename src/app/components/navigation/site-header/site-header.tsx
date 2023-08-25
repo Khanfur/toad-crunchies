5e05860a4eacafc4438231c9e4a463174006610c
Brandon <brandon.tromp@gmail.com>
2023-08-25 11:17:43 +0200

    added more css

--- src/app/components/navigation/site-header/site-header.tsx
+++ src/app/components/navigation/site-header/site-header.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image'
-import logoColor from '../../../assets/images/logo-color.svg'
+import logoColor from '@app-images/logo-color.svg'
 import styles from './styles.module.scss'
 
 export default function SiteHeader() {
     return (
-        <main>
-            <Image priority
-                   src={ logoColor }
-                   width={ 128 }
-                   height={ 128 }
-                   alt="Toad Crunchies Recipes"
-                   className={ styles.logo }
-            />
-        </main>
+        <div>
+            <div>
+                <Image priority
+                       src={ logoColor }
+                       width={ 128 }
+                       height={ 128 }
+                       alt="Toad Crunchies Recipes"
+                       className={ styles.logo }
+                />
+            </div>
+        </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,23 @@ import type { Metadata } from 'next'
 import '@fontsource-variable/montserrat'
 
 export const metadata: Metadata = {
-  title: 'Toad crunchies',
-  description: 'Toad crunchies recipes. The flavour is on your side.',
+    title: 'Toad crunchies',
+    description: 'Toad crunchies recipes. The flavour is on your side.',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-    <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-    </head>
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" href="/favicon.ico" sizes="any"/>
+        </head>
+        <body className="container">
+        { children }
+        </body>
+        </html>
+    )
 }
